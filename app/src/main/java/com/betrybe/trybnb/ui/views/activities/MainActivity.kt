0f1846c0ee2bcc359bf7d8031c7ffbf2b6036320
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2024-03-21 14:53:09 -0300

    req 3

--- app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
@@ -3,11 +3,50 @@ package com.betrybe.trybnb.ui.views.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.betrybe.trybnb.R
+import com.betrybe.trybnb.ui.views.fragments.CreateReservationFragment
+import com.betrybe.trybnb.ui.views.fragments.ProfileFragment
+import com.betrybe.trybnb.ui.views.fragments.ReservationFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val mNavBar: BottomNavigationView by lazy {findViewById(R.id.navigation_bottom_view)}
+
+    private val mFragmentProfile = ProfileFragment()
+    private val mFragmentReservation = ReservationFragment()
+    private val mFragmentCreateReservation = CreateReservationFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mNavBar.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.reservation_menu_item -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.main_fragment_container, mFragmentReservation)
+                        .addToBackStack(null)
+                        .commit()
+                    true
+                }
+                R.id.create_reservation_menu_item -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.main_fragment_container, mFragmentCreateReservation)
+                        .addToBackStack(null)
+                        .commit()
+                    true
+                }
+                R.id.profile_menu_item -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.main_fragment_container, mFragmentProfile)
+                        .addToBackStack(null)
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
